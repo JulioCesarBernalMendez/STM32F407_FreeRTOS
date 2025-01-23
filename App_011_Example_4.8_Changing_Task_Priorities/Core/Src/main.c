b47f2dcffe13d593d520bfe45b910a3350dbdcd2
JulioCesarBernalMendez <juliocesarbernalmendez@hotmail.com>
2025-01-23 11:02:09 -0600

    Added SEGGER SystemView to App_011

--- App_011_Example_4.8_Changing_Task_Priorities/Core/Src/main.c
+++ App_011_Example_4.8_Changing_Task_Priorities/Core/Src/main.c
@@ -121,6 +121,14 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+  /* enable CYCCNT (Cycle Count, needed for SEGGER SystemView) in DWT_CTRL register */
+  DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+
+  /* initialize and configure SEGGER SystemView */
+  SEGGER_SYSVIEW_Conf();
+
+  /* start recording SEGGER SystemView events */
+  SEGGER_SYSVIEW_Start();
 
   /* Create the first task at priority 2. The task parameter is not used and set to NULL.
      The task handle is also not used so is also set to NULL */
