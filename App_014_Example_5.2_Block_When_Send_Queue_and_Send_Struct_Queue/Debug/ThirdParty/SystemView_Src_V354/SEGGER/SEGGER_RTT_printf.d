8080ec1e2c413a19765b02715c9f931288253faf
JulioCesarBernalMendez <juliocesarbernalmendez@hotmail.com>
2025-01-31 11:19:49 -0600

    Added SEGGER SystemView to App_018

--- App_014_Example_5.2_Block_When_Send_Queue_and_Send_Struct_Queue/Debug/ThirdParty/SystemView_Src_V354/SEGGER/SEGGER_RTT_printf.d
+++
@@ -1,6 +0,0 @@
-ThirdParty/SystemView_Src_V354/SEGGER/SEGGER_RTT_printf.o: \
- ../ThirdParty/SystemView_Src_V354/SEGGER/SEGGER_RTT_printf.c \
- ../ThirdParty/SystemView_Src_V354/SEGGER/SEGGER_RTT.h \
- C:/Users/julio/Downloads/STM32F407_FreeRTOS/App_014_Example_5.2_Block_When_Send_Queue_and_Send_Struct_Queue/ThirdParty/SystemView_Src_V354/Config/SEGGER_RTT_Conf.h
-../ThirdParty/SystemView_Src_V354/SEGGER/SEGGER_RTT.h:
-C:/Users/julio/Downloads/STM32F407_FreeRTOS/App_014_Example_5.2_Block_When_Send_Queue_and_Send_Struct_Queue/ThirdParty/SystemView_Src_V354/Config/SEGGER_RTT_Conf.h:
